f6f08b6ac84f13048ea0dcc2a678269e86a73018
Ergon Zamarian <ergonzamarian@gmail.com>
2022-07-03 21:58:16 -0300

    Adicionando AR.Domain e AR.Data / Adicionando referências ao entityframework e mapeando domain como dbset

---
+++ AR.Data/ContextoPrincipal.cs
@@ -0,0 +1,16 @@
+﻿using AR.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AR.Data
+{
+    public class ContextoPrincipal : DbContext
+    {
+        public ContextoPrincipal(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Cliente
+    }
+}
---
+++ AR.Domain/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    public class Cliente
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage ="O campo {0} é obrigatório")]
+        [MaxLength(50, ErrorMessage ="O campo {0} deve ter no máximo {1} caracteres")]
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+
+
+    }
+}
